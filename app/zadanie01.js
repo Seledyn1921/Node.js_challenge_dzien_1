4ecb0e6b087d86990479be6edf2b433e6cae4d7b
Seledyn1921 <106825926+Seledyn1921@users.noreply.github.com>
2022-06-03 18:37:03 +0200

    Update zadanie01.js

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,8 @@
-Rafal Zielinski
+const text = "Witam";
+setTimeout(()=>{się}, 1000);
+setTimeout(()=>{z}, 2000);
+setTimeout(()=>{programem}, 3000);
+setTimeout(()=>{Node.js}, 4000);
+setTimeout(()=>{w konsoli}, 5000);
+setTimeout(()=>{i korzystam}, 6000);
+setTimeout(()=>{z funkcji czasu!}, 7000);
