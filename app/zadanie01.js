868f467356e5210ecbf4867514e7a9e2549d19b0
Seledyn1921 <106825926+Seledyn1921@users.noreply.github.com>
2022-06-04 06:14:21 +0200

    Update zadanie01.js

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,15 +1,2 @@
-//zadanie01
 console.log(`Rafal Zielinski`);
 setTimeout(()=>{console.log(`wita się z node.js`)}, 5000);
-
-//zadanie02
-console.log(`Witam`);
-setTimeout(()=>{console.log(`z`)}, 2000);
-setTimeout(()=>{console.log(`programem`)}, 3000);
-setTimeout(()=>{console.log(`Node.js`)}, 4000);
-setTimeout(()=>{console.log(`w konsoli`)}, 5000);
-setTimeout(()=>{console.log(`i korzystam`)}, 6000);
-setTimeout(()=>{console.log(`z funkcji czasu`)}, 7000);
-
-
-
