989321d3b0fdb1df365511b7b27a59963ab9b9c7
Seledyn1921 <106825926+Seledyn1921@users.noreply.github.com>
2022-06-04 07:05:38 +0200

    Update zadanieDnia.js

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,7 @@
-//Twój kod
+setTimeout(()=>{console.log(`2`)}, 2000);
+setTimeout(()=>{console.log(`1`)}, 1000);
+setTimeout(()=>{console.log(`4`)}, 4000);
+setTimeout(()=>{console.log(`8`)}, 8000);
+setTimeout(()=>{console.log(`10`)}, 10000);
+setTimeout(()=>{console.log(`8`)}, 8000);
+setTimeout(()=>{console.log(`4`)}, 4000);
